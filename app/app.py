ba3e32515c27a35085e08abc0840a233455ebe58
Michael Zhang <michael13162@gmail.com>
2017-11-17 12:57:01 -0700

    Added endpoints

--- app/app.py
+++ app/app.py
@@ -1,15 +1,27 @@
+import json
 from flask import Flask
-app = Flask(__name__)
+from flask import request
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
-
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+@app.route('/api/account/register', methods=['POST'])
+def register():
+    account_info = request.form
+    print(account_info)
+
+@app.route('/api/account/user', methods=['GET'])
+def user():
+    js = []
+    js.append("Yo Will")
+    return Response(json.dumps(js), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
