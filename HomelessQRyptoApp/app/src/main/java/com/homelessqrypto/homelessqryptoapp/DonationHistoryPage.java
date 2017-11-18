d5b1d966da8ce8ad2ad9f9dd1728f2e4942e1e4a
Jeremy <jeremy.r.whipple@gmail.com>
2017-11-17 21:04:21 -0700

    Added android app files

---
+++ HomelessQRyptoApp/app/src/main/java/com/homelessqrypto/homelessqryptoapp/DonationHistoryPage.java
@@ -0,0 +1,13 @@
+package com.homelessqrypto.homelessqryptoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonationHistoryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_history_page);
+    }
+}
